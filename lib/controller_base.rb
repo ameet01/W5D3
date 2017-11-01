f0493105941948ccdec8463f1354b7d0c28f2a61
Ameet Vadhia <ameet01@gmail.com>
2017-11-01 14:40:47 -0700

    implement controllerbase

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -8,25 +9,41 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'double render' if already_built_response?
+    @res['Location'] = url
+    @res.status = 302
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise 'double render' if already_built_response?
+    @res.write(content)
+    @res['Content-Type'] = content_type
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.name.underscore
+    template = File.read("views/#{controller_name}/#{template_name}.html.erb")
+    content = ERB.new(template).result(binding)
+    render_content(content, 'text/html')
   end
 
   # method exposing a `Session` object
@@ -37,4 +54,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
